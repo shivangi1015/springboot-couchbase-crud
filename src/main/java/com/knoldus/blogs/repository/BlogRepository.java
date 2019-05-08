e9a9428300c65fdf4763d65d030d3881a3631115
shivangi Gupta <shivangi.gupta@knoldus.in>
2019-05-08 16:53:02 +0530

    Added more crud oprations.

--- src/main/java/com/knoldus/blogs/repository/BlogRepository.java
+++ src/main/java/com/knoldus/blogs/repository/BlogRepository.java
@@ -3,7 +3,13 @@ package com.knoldus.blogs.repository;
 import com.knoldus.blogs.models.Blog;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BlogRepository extends CrudRepository<Blog, String> {
-
-
+    
+    long countByAuthor(String author);
+    
+    long deleteBytopicAndAuthor(String title, String author);
+    
+    List<Blog> findByTags(String tag);
 }
